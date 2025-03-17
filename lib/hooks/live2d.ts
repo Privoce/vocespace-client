b99f9a35ae8435823ccc48fe7653970bba7bd5ed
syf20020816 <syf20020816@outlook.com>
2025-03-17 01:22:23 -0700

    add live2d

---
+++ lib/hooks/live2d.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import * as Define from "../live2d/render/Define";
+import { Delegate } from "../live2d/render/Delegate";
+
+export default function useLive2D() {
+  return useEffect(() => {
+    Delegate.releaseInstance();
+
+    if (Delegate.getInstance().initialize() === false) {
+      return;
+    }
+
+    Delegate.getInstance().run();
+
+    if (Define.CanvasSize === "auto") {
+      Delegate.getInstance().onResize();
+    }
+
+    window.addEventListener("resize", () => {
+      Delegate.getInstance().onResize();
+    });
+  }, []);
+}
+
