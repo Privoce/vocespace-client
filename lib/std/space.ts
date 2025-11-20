d6cdbe73fad6afd7bfa097792389af1c5f7fd266
syf20020816 <syf20020816@outlook.com>
2025-11-20 21:49:10 +0800

    [feat]: 0.4.6

--- lib/std/space.ts
+++ lib/std/space.ts
@@ -405,7 +405,7 @@ export const DEFAULT_SPACE_INFO = (startAt: number): SpaceInfo => ({
 });
 
 export const DEFAULT_PARTICIPANT_SETTINGS: ParticipantSettings = {
-  version: '0.4.5',
+  version: '0.4.6',
   name: '',
   volume: 100,
   blur: 0.0,
